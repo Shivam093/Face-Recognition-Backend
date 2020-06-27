2f370323d81aa967f48768c2aa963be19daa45e2
Shivam093 <shuklashivam1971@gmail.com>
2020-06-27 12:57:58 +0530

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image =require('./controllers/image');
 const db =knex({
 	client: 'pg',
 	connection:{
-		host: 'postgresql-objective-81623',
-		user: 'postgres',
-		password: 'Shivam123',
-		database: 'brain'
+		host: 'process.env.DATABASE_URL',
+		ssl: true
 	}
 });
 
